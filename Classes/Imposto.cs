e3d3ab749542c44affca02cc74cc0dd5776796c7
JoaoRossi49 <joao.rossi.figueiredo@gmail.com>
2024-03-08 20:32:51 -0300

    feat: propriedades auto-implementadas e métodos construtores para impostos

--- Classes/Imposto.cs
+++ Classes/Imposto.cs
@@ -1,48 +1,54 @@
-
 using System;
-using DDP;
 
-namespace DDP{
+namespace DDP {
 
     public class Imposto {
 
-        public string descricao;        
-        public double porcentagem;
+        public string Descricao { get; set; }
+        public double Porcentagem { get; set; }
 
-        public string Descricao { get => descricao; set => descricao = value; }
-        public double Porcentagem { get => porcentagem; set => porcentagem = value; }
-
-        public virtual double calculaImposto(double valorInicial){
-            return (valorInicial * this.porcentagem);
+        public virtual double CalculaImposto(double valorInicial) {
+            return valorInicial * Porcentagem;
         }
-
     }
 
-    public class ICMS : Imposto{
-        public override double calculaImposto(double valorInicial){
+    public class ICMS : Imposto {
+        public ICMS() {
             Porcentagem = 0.12;
-            return base.calculaImposto(valorInicial);
+        }
+
+        public override double CalculaImposto(double valorInicial) {
+            return base.CalculaImposto(valorInicial);
         }
     }
 
-    public class PIX : Imposto{
-        public override double calculaImposto(double valorInicial){
+    public class PIX : Imposto {
+        public PIX() {
             Porcentagem = 0.16;
-            return base.calculaImposto(valorInicial);
+        }
+
+        public override double CalculaImposto(double valorInicial) {
+            return base.CalculaImposto(valorInicial);
         }
     }
 
-    public class COFINS : Imposto{
-        public override double calculaImposto(double valorInicial){
+    public class COFINS : Imposto {
+        public COFINS() {
             Porcentagem = 0.18;
-            return base.calculaImposto(valorInicial);
+        }
+
+        public override double CalculaImposto(double valorInicial) {
+            return base.CalculaImposto(valorInicial);
         }
     }
 
-    public class ISS : Imposto{
-        public override double calculaImposto(double valorInicial){
+    public class ISS : Imposto {
+        public ISS() {
             Porcentagem = 0.11;
-            return base.calculaImposto(valorInicial);
+        }
+
+        public override double CalculaImposto(double valorInicial) {
+            return base.CalculaImposto(valorInicial);
         }
     }
 }
